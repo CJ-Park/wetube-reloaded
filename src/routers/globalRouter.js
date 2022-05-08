6238fcd3204222cba962dbb2648f8948cf428d3a
JOE1534 <king01286@naver.com>
2022-05-08 14:54:16 +0900

    Exports

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
+import {join} from "../controllers/userController";
+import {recommend} from "../controllers/videoController";
+
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
+globalRouter.get("/", recommend);
+globalRouter.get("/join", join);
 
 export default globalRouter;
