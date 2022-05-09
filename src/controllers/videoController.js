49572c6cb9d925f14aa0ade5c2929a4697dc1cf8
JOE1534 <king01286@naver.com>
2022-05-09 22:53:52 +0900

    Use Pug and Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,11 @@
-export const recommend = (req, res) => res.send("Home Page Videos");
+export const recommend = (req, res) => res.render("home");
 export const watch = (req, res) => {
     console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
+    return res.render("watch");
 }
 export const edit = (req, res) => {
     console.log(req.params);
-    return res.send("Edit Video");
+    return res.render("edit");
 }
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
