efc4bf0ab9da4f69c24b942e1b0b1d731fe43880
JOE1534 <king01286@naver.com>
2022-05-09 23:07:24 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,8 @@
 export const recommend = (req, res) => res.render("home");
-export const watch = (req, res) => {
-    console.log(req.params);
-    return res.render("watch");
-}
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.render("edit");
-}
+export const watch = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
     return res.send("Delete Video");
 }
