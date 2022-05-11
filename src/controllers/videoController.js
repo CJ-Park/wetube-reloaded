b82e6fefc682f03ee290711b6ba4c1e7c4304644
JOE1534 <king01286@naver.com>
2022-05-12 00:01:32 +0900

    Array Dateabase #2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
         rating: 5,
         comments: 2,
         createdAt: "2 minutes ago",
-        views: 59,
+        views: 1,
         id: 1,
     },
     {
@@ -32,7 +32,7 @@ export const recommend = (req, res) => {
 export const watch = (req, res) => {
     const {id} = req.params;
     const video = videos[id-1];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`});
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
 };
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search Video");
