4619a0a1f4e7f0d1bcf27142bcacf0731daec767
JOE1534 <king01286@naver.com>
2022-07-13 18:28:17 +0900

    Create / Validate Account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,41 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+    return res.render("join", {pageTitle: "Join"});
+};
+
+export const postJoin = async(req, res) => {
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "Join";
+    if(password !== password2) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMsg: "Password confirm failed",
+        });
+    }
+    const exists = await User.exists({$or: [{username}, {email}]});
+    if(exists) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMsg: "This username/email is already in use"
+        });
+    }
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
+
 export const edit = (req, res) => res.send("Edit User");
+
 export const remove = (req, res) => res.send("Delete User");
+
 export const login = (req, res) => res.send("Log In");
+
 export const logout = (req, res) => res.send("Log Out");
+
 export const see = (req, res) => res.send("See User Profile");
