1501d436fe4b827cd4acb5f3a2bc4156be47f123
JOE1534 <king01286@naver.com>
2022-07-18 19:41:38 +0900

    Add Github Login & Logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {edit, remove, logout, see} from "../controllers/userController";
+import {edit, logout, startGithubLogin, finishGithubLogin, see} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
 userRouter.get("/logout", logout);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
